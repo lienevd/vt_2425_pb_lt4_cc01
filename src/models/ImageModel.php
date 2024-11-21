31462dec234b7baa62ffae7f295ebde19fb9835e
Jacco Koridon <jacco.koridon@student.hu.nl>
2024-11-21 18:48:12 +0100

    Laatste dingen

--- src/models/ImageModel.php
+++ src/models/ImageModel.php
@@ -2,14 +2,11 @@
 
 namespace Src\Models;
 
-use Src\Enums\ResponseTypeEnum;
-
 class ImageModel extends BaseModel
 {
     public function AddImage($image, $category): bool
     {
         $this->db->query('INSERT INTO images (image, category) VALUES (:image, :category);');
-        $data = array();
 
         $this->db->bindParams([
             ['image', $image, \PDO::PARAM_LOB],
