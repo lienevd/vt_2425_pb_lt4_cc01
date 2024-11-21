684244e949c43709b8ee35eff6c6d4e601412ebb
Jacco <jaccokoridon@gmail.com>
2024-11-21 18:17:12 +0100

    ImageModel aanpassing

--- src/models/ImageModel.php
+++ src/models/ImageModel.php
@@ -11,15 +11,10 @@ class ImageModel extends BaseModel
         $this->db->query('INSERT INTO images (image, category) VALUES (:image, :category);');
         $data = array();
 
-        $data[0][0] = 'image';
-        $data[0][1] = $image;
-        $data[0][2] = \PDO::PARAM_LOB;
-
-        $data[1][0] = 'category';
-        $data[1][1] = $category;
-        $data[1][2] = \PDO::PARAM_STR;
-
-        $this->db->bindParams($data);
+        $this->db->bindParams([
+            ['image', $image, \PDO::PARAM_LOB],
+            ['category', $category, \PDO::PARAM_STR]
+        ]);
         $this->db->execute();
 
         return ResponseTypeEnum::OK;
