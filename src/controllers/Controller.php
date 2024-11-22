2cb7dd431682e586992e6759512e716b3ebf9e33
Roy Vermeulen <rvermeulen@eljakim.nl>
2024-11-22 10:53:02 +0100

    images uit db ophalen en sturen in json

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -4,6 +4,7 @@ namespace Src\Controllers;
 
 use Src\Enums\ResponseTypeEnum;
 use Src\Http\Response;
+use Src\Models\ImageModel;
 
 final class Controller
 {
@@ -14,7 +15,12 @@ final class Controller
 
     public function getImages(string $category): Response
     {
-        echo $category;
-        return new Response(ResponseTypeEnum::OK);
+        $imageModel = new ImageModel;
+        $images = [];
+        foreach ($imageModel->getImages($category) as $image) {
+            $images[] = $image['image'];
+        }
+
+        return new Response(ResponseTypeEnum::OK, json_encode($images));
     }
 }
--- src/models/ImageModel.php
+++ src/models/ImageModel.php
@@ -2,6 +2,8 @@
 
 namespace Src\Models;
 
+use Src\Collections\ImageCollection;
+
 class ImageModel extends BaseModel
 {
     public function AddImage(string $image, string $category): bool
@@ -13,5 +15,14 @@ class ImageModel extends BaseModel
             ['category', $category, \PDO::PARAM_STR]
         ]);
         return $this->db->execute();
+    }
+
+    public function getImages(string $category): ?ImageCollection
+    {
+        return $this->db->query('SELECT * FROM images WHERE category = :category')
+            ->bindParams([
+                [':category', $category, \PDO::PARAM_STR]
+            ])
+            ->fetchAssoc(new ImageCollection());
     }
 }
