86df618e4fe2241892e51db52109e25738490ff8
Jacco Koridon <jacco.koridon@student.hu.nl>
2024-12-11 14:19:17 +0100

    Afbeeldingen die wel gekoppeld zijn aan de hint maar niet bedoeld zijn om te laden, worden niet meer geladen

--- src/models/ImageModel.php
+++ src/models/ImageModel.php
@@ -22,7 +22,7 @@ class ImageModel extends BaseModel
 
     public function getImages(int $hint_id, string $category, int $amount): ?string
     {
-        $this->db->query('SELECT * FROM hint_image AS hi JOIN images AS i ON hi.image_id = i.id WHERE hint_id = :hint_id AND i.category = :category LIMIT 4;');
+        $this->db->query('SELECT * FROM hint_image AS hi JOIN images AS i ON hi.image_id = i.id WHERE hint_id = :hint_id AND i.category = :category ORDER BY RAND() LIMIT 4;');
         $this->db->bindParams([
             [':hint_id', $hint_id, \PDO::PARAM_INT],
             [':category', $category, \PDO::PARAM_STR]
@@ -40,21 +40,38 @@ class ImageModel extends BaseModel
             }
         }
 
-        $amountLeft = $amount - $this->db->rowCount();
-
         $queryNot = "";
 
         foreach ($image_ids as $image_id) {
             $queryNot .= " AND NOT id = $image_id";
         }
 
-        $this->db->query('SELECT * FROM images WHERE category = :category'.$queryNot.' ORDER BY RAND() LIMIT :amount;');
+        $this->db->query('SELECT * FROM images WHERE category = :category'.$queryNot.' ORDER BY RAND();');
         $this->db->bindParams([
-            [':category', $category, \PDO::PARAM_STR],
-            [':amount', $amountLeft, \PDO::PARAM_INT]
+            [':category', $category, \PDO::PARAM_STR]
         ]);
 
         foreach ($this->db->fetchAssoc()->getItems() as $image) {
+
+            if (count($results) >= $amount) {
+                break;
+            }
+
+            if (in_array($image['id'], $image_ids)) {
+                continue;
+            }
+
+            $this->db->query('SELECT * FROM hint_image WHERE image_id = :image_id AND hint_id = :hint_id;');
+            $this->db->bindParams([
+                [':image_id', $image['id'], \PDO::PARAM_INT],
+                [':hint_id', $hint_id, \PDO::PARAM_INT]
+            ]);
+            $this->db->execute();
+
+            if ($this->db->rowCount() > 0) {
+                continue;
+            }
+            
             array_push($results, ['image' => $image['image'], 'id' => $image['id']]);
         }
 
